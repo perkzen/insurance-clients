9dfd20413018211b29805a489f153c433a352079
perkzen <perko.domen@gmail.com>
2022-07-03 21:38:21 +0200

    feat: update table

--- frontend/packages/manage-clients/ClientsTable/ClientsTable.tsx
+++ frontend/packages/manage-clients/ClientsTable/ClientsTable.tsx
@@ -4,16 +4,13 @@ import { InsuranceClient } from 'shared-types';
 import { ITableHeader } from 'ui/components/Table/Table';
 import { EmptyTable } from 'ui';
 import { useQuery } from 'react-query';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { BE_URL } from '../axios';
-import { format } from 'date-fns';
 
 const headers: ITableHeader<InsuranceClient>[] = [
   { label: 'Firstname', accessor: 'firstname' },
   { label: 'Lastname', accessor: 'lastname' },
   { label: 'Email', accessor: 'email' },
-  { label: 'Gender', accessor: 'gender' },
-  { label: 'Birthday', accessor: 'birthday' },
 ];
 
 interface ClientsTableProps {
@@ -23,15 +20,7 @@ interface ClientsTableProps {
 
 export const ClientsTable: FC<ClientsTableProps> = ({ header, onRowClick }) => {
   const { data, isLoading } = useQuery('clients', () =>
-    axios.get(BE_URL).then(
-      (res) =>
-        res.data.map((item: InsuranceClient) => {
-          return {
-            ...item,
-            birthday: format(new Date(item.birthday), 'dd/MM/yyyy'),
-          };
-        }) as InsuranceClient[]
-    )
+    axios.get(BE_URL).then((res) => res.data as InsuranceClient[])
   );
 
   return (
