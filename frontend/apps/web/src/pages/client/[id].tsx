947ca5d1a15bd80d3f643c70509c2b5f28889022
perkzen <perko.domen@gmail.com>
2022-07-04 18:18:40 +0200

    feat: add filter to table

--- frontend/apps/web/src/pages/client/[id].tsx
+++ frontend/apps/web/src/pages/client/[id].tsx
@@ -8,7 +8,6 @@ const ClientsForm = () => {
 
   return (
     <div className={'flex flex-col justify-center items-center'}>
-      <h1 className={'text-2xl font-bold mb-5'}>Add client</h1>
       <InsuranceClientForm clientId={parseInt(id)} />
     </div>
   );
